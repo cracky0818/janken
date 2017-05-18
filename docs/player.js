802e6f72350673a9783004f6e072b77c96269aef
cracky0818 <panda12198@hotmail.com>
2017-05-18 11:43:24 +0900

    三つのプレーヤーに勝つ関数

--- docs/player.js
+++ docs/player.js
@@ -8,16 +8,42 @@ const PA = 2;
 
 let matches = 0;
 
-function action(){
+// チョキ大好きマンに勝つ方法
+function actionAgainstChokiDaisukiman(){
   return GU;
 }
+
+// 表か裏マンに勝つ方法
+function actionAgainstOmoteUra(){
+  matches = matches + 1;
+  const isEven = matches % 2 == 0;
+  let result = PA;
+  if(isEven){
+    result = GU;
+  }
+  return result;
+}
+
+// 帰ってきた表か裏マンに勝つ方法
 function actionAgainstOmoteUra2(){
   matches = matches + 1;
+  const isEven = matches % 3 == 0;
   let result = GU;
+  if(isEven){
+    result = CHOKI;
+  }
   return result;
 }
 
 function action(oppornent){
-let result = actionAgainstOmoteUra2();
+console.log(oppornent);
+let result = GU;
+if(oppornent == "fighter::choki-lover"){
+  result = actionAgainstChokiDaisukiman();
+} else if(oppornent == "fighter::odd-even")  {
+  result = actionAgainstOmoteUra();
+} else {
+  result = actionAgainstOmoteUra2();
+}
 return result;
 }

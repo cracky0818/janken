9ca116639417610c5c399cbd28216356b5171d80
cracky0818 <panda12198@hotmail.com>
2017-05-18 12:42:03 +0900

    四つのプレーヤーに勝つ

--- docs/player.js
+++ docs/player.js
@@ -15,7 +15,6 @@ function actionAgainstChokiDaisukiman(){
 
 // 表か裏マンに勝つ方法
 function actionAgainstOmoteUra(){
-  matches = matches + 1;
   const isEven = matches % 2 == 0;
   let result = PA;
   if(isEven){
@@ -26,7 +25,6 @@ function actionAgainstOmoteUra(){
 
 // 帰ってきた表か裏マンに勝つ方法
 function actionAgainstOmoteUra2(){
-  matches = matches + 1;
   const isEven = matches % 3 == 0;
   let result = GU;
   if(isEven){
@@ -35,15 +33,34 @@ function actionAgainstOmoteUra2(){
   return result;
 }
 
+// 順番に出していくマン
+function actionAgainstRotation(){
+  const toReturnChoki = matches % 3 == 0;
+  const toReturnPa = matches % 3 == 1;
+  let result = GU;
+  if(toReturnChoki){
+    result = CHOKI;
+  } else if(toReturnPa){
+      result = PA;
+  } else {
+  result = GU;
+  }
+  return result;
+  }
+
 function action(oppornent){
 console.log(oppornent);
+matches = matches + 1;
 let result = GU;
 if(oppornent == "fighter::choki-lover"){
   result = actionAgainstChokiDaisukiman();
 } else if(oppornent == "fighter::odd-even")  {
   result = actionAgainstOmoteUra();
+} else if(oppornent == "fighter::rotation"){
+    result = actionAgainstRotation();
 } else {
   result = actionAgainstOmoteUra2();
-}
+ }
 return result;
 }
+
